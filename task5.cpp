af8918915f85789d420f4512a33c0d10f0b3e928
yoyom4s1er <80907085+yoyom4s1er@users.noreply.github.com>
2021-10-23 12:40:09 +0300

    Update task5.cpp

--- task5.cpp
+++ task5.cpp
@@ -4,29 +4,29 @@
 #include "task5.h"
 
 int root1(int hours, int minutes) {
-    return hours * SECONDS_IN_HOUR + minutes * SECONDS_IN_MINUTES;
+    return hours * seconds_in_hour + minutes * seconds_in_minute;
 }
 
 int root2(int hours, int minutes) throw() {
-    return hours * SECONDS_IN_HOUR + minutes * SECONDS_IN_MINUTES;
+    return hours * seconds_in_hour + minutes * seconds_in_minute;
 }
 
 int root3(int hours, int minutes) throw(std::invalid_argument) {
     if (hours < 0 or minutes < 0)
         throw std::invalid_argument("часы или минуты меньше нуля");
-    return hours * SECONDS_IN_HOUR + minutes * SECONDS_IN_MINUTES;
+    return hours * seconds_in_hour + minutes * seconds_in_minute;
 }
 
 int root4_1(int hours, int minutes) throw(except1) {
     if (hours < 0 or minutes < 0)
         throw except1();
-    return hours * SECONDS_IN_HOUR + minutes * SECONDS_IN_MINUTES;
+    return hours * seconds_in_hour + minutes * seconds_in_minute;
 }
 
 int root4_2(int hours, int minutes) throw(except2) {
     if (hours < 0 or minutes < 0)
         throw except2("часы или минуты меньше нуля");
-    return hours * SECONDS_IN_HOUR + minutes * SECONDS_IN_MINUTES;
+    return hours * seconds_in_hour + minutes * seconds_in_minute;
 }
 
 int root4_3(int hours, int minutes) throw(except3) {
@@ -34,11 +34,11 @@ int root4_3(int hours, int minutes) throw(except3) {
         throw except3("Неверный аргумент(часы): ", hours);
     else if (minutes < 0)
         throw except3("Неверный аргумент(минуты): ", minutes);
-    return hours * SECONDS_IN_HOUR + minutes * SECONDS_IN_MINUTES;
+    return hours * seconds_in_hour + minutes * seconds_in_minute;
 }
 
 void helper(int (*func)(int, int)) {
-    double hours, minutes;
+    int hours, minutes;
     std::cout << "Введите часы ";
     std::cin >> hours;
     std::cout << "Введите минуты ";
